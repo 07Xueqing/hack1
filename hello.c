1715461033129d5817b6cbd18c24b814a638f9f7
IIGravityII <42561496+IIGravityII@users.noreply.github.com>
2018-08-23 15:00:09 -0500

    Added name to partners code

--- hello.c
+++ hello.c
@@ -8,7 +8,7 @@ Date:08/23/2018
 
 int main(void){
 
-	printf("Xueqing zhao Electrical Engineering ");
+	printf("Xueqing zhao Electrical Engineering; Sean Hollibaugh Mechanical Engineering ");
 
 	return 0;
 
