41c5371407da32c3f6b9cd01c05574145853d8de
Xueqing Zhao <zhao071897@unlstudent.com>
2018-08-23 12:57:30 -0700

    Change name Xueqing Zhao

---
+++ hello.c
@@ -0,0 +1,15 @@
+/*
+Author: Xueqing Zhao  Sean Hollibaugh
+
+Date:08/23/2018
+
+* Simple hello world program
+*/
+
+int main(void){
+
+	printf("Xueqing zhao Electrical Engineering ");
+
+	return 0;
+
+}
